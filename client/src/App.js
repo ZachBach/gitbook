7e31e55bbdd5f4419439536b0913c7e488577efe
Zach Auerbach <auernachzach@gmail.com>
2020-05-27 15:55:34 -0700

    added dummy login/sign up and also setup express server and mongoDB schema

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
+import SignUp from './components/pages/SignUp';
+import SignIn from './components/pages/SignIn';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={SignUp} />
+                <Route exact path='/login' component={SignIn} />
+                <Route exact path='/home' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
+
+export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/home'>Home</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/'>Sign up</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: 'GitBook',
+  icon: 'fab fa-github',
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+export default Navbar;
