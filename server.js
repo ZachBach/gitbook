eab6a1dc74587415d9013820909c6ef97e0c1029
Zach Auerbach <auernachzach@gmail.com>
2020-06-01 15:51:58 -0700

    changed the Link in signin component to a onClick function with an axios request. Still an issue with CORS and the proxy not working properly

--- server.js
+++ server.js
@@ -32,47 +32,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const GitHubStrategy = require('passport-github2').Strategy;
-
-const GITHUB_CLIENT_ID = 'cd53ae7fdb8ecb986bf6';
-const GITHUB_CLIENT_SECRET = 'c21e415068681ae73258bd60a46d5fefc393d817';
-const GITHUB_CALLBACK_URL = '/auth/github/callback';
-
-passport.use(
-  new GitHubStrategy(
-    {
-      clientID: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: GITHUB_CALLBACK_URL,
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(accessToken, refreshToken, profile);
-      return cb(null, profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/github',
-  passport.authenticate('github', { scope: ['user:email'] })
-);
-
-app.get(
-  '/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('http://localhost:3000/home');
-  }
-);
-passport.serializeUser(function (user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-  cb(null, obj);
-});
-
 // Define Routes
 // app.use('/api/users', require('./routes/api/users'));
 // app.use('/api/auth', require('./routes/api/auth'));
@@ -81,8 +40,8 @@ passport.deserializeUser(function (obj, cb) {
 
 app.use(
   cors({
-    origin: 'http://localhost:3000', // allow to server to accept request from different origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: 'http://localhost:3001', // allow to server to accept request from different origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,FETCH',
     credentials: true, // allow session cookie from browser to pass through
   })
 );
