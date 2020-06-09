3d61658c9f93c2da05cdc3281ad9305cc2dfbcd5
Zach Auerbach <auernachzach@gmail.com>
2020-06-08 18:22:07 -0700

    Set up the context api for the wall posts so that it's grabbing the input value

--- client/src/components/wallpost/MessageBox.js
+++ client/src/components/wallpost/MessageBox.js
@@ -1,17 +1,32 @@
-import React from "react"
-import InputContext from "../../context/input/inputContext"
+import React, { useState, useContext } from 'react';
+import WallMsgContext from '../../context/WallMessages/wallMsgContext';
 
 const MessageBox = () => {
+  const [wallposting, setWallPosting] = useState([]);
+  const wallMsgContext = useContext(WallMsgContext);
 
-    return (
-        <div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">githubHanle</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">
-                </textarea>
-            </div>
-        </div>
-    )
-}
+  const [state, setState] = useState('');
+
+  const onChange = (e) => {
+    const inputValue = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: inputValue,
+    });
+  };
+
+  return (
+    <div>
+      <div class='form-group'>
+        <label for='exampleFormControlTextarea1'>githubHandle</label>
+        <textarea
+          onChange={onChange}
+          class='form-control'
+          id='exampleFormControlTextarea1'
+          rows='3'></textarea>
+      </div>
+    </div>
+  );
+};
 
 export default MessageBox;
--- client/src/components/wallpost/PostBox.js
+++ client/src/components/wallpost/PostBox.js
@@ -1,15 +1,16 @@
-import React from "react"
+import React from 'react';
 
 const PostBox = () => {
-
-    return (
-        <div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">githubHanle</label>
-                <div class="form-control" id="exampleFormControlTextarea1" rows="3">Message</div>
-            </div>
+  return (
+    <div>
+      <div class='form-group'>
+        <label for='exampleFormControlTextarea1'>githubHandle</label>
+        <div class='form-control' id='exampleFormControlTextarea1' rows='3'>
+          Message
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default PostBox;
