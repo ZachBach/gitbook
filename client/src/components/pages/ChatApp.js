74aabd25b630058a43e92ae9722ad272f76bd0dd
BrianU <brian.ulatowski@gmail.com>
2020-06-15 21:58:04 -0700

    working on chat username

--- client/src/components/pages/ChatApp.js
+++ client/src/components/pages/ChatApp.js
@@ -5,41 +5,9 @@ import Input from '../../Input';
 import { fakeAuth } from '../privateroute/PrivateRoute';
 import SignIn from './SignIn';
 
-// function randomName() {
-//   const adjectives = [
-//     "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
-//     "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
-//     "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
-//     "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long",
-//     "late", "lingering", "bold", "little", "morning", "muddy", "old", "red",
-//     "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering",
-//     "withered", "wild", "black", "young", "holy", "solitary", "fragrant",
-//     "aged", "snowy", "proud", "floral", "restless", "divine", "polished",
-//     "ancient", "purple", "lively", "nameless"
-//   ];
-//   const nouns = [
-//     "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
-//     "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
-//     "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
-//     "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
-//     "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
-//     "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
-//     "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
-//     "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog",
-//     "smoke", "star"
-//   ];
-//   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-//   const noun = nouns[Math.floor(Math.random() * nouns.length)];
-//   return adjective + noun;
-// }
-
 function randomColor() {
   return '#' + Math.floor(Math.random() * 0xffffff).toString(16);
 }
-
-// SignIn(data)
-// console.log(data)
-// console.log(SignIn(data));
 
 const chatUsername = async () => {
   const getCurrentUser = await fetch('/api/currentuser', {
@@ -59,14 +27,14 @@ const chatUsername = async () => {
 };
 
 class ChatApp extends Component {
-  componentDidMount() {
-    chatUsername().then((gitHubHandle) => {
-      // const gitHubHandle = chatUsername();
-      this.setState({
-        member: { username: gitHubHandle.CurrentUserGitHubHandle },
-      });
-    });
-  }
+  // componentDidMount() {
+    // chatUsername().then((gitHubHandle) => {
+    //   // const gitHubHandle = chatUsername();
+    //   this.setState({
+    //     member: { username: gitHubHandle.CurrentUserGitHubHandle },
+    //   });
+    // });
+  // }
   state = {
     messages: [],
     member: {
@@ -86,7 +54,7 @@ class ChatApp extends Component {
       }
       const member = { ...this.state.member };
       member.id = this.drone.clientId;
-      // this.setState({ member });
+      this.setState({ member });
     });
     const room = this.drone.subscribe('observable-room');
     room.on('data', (data, member) => {
@@ -113,6 +81,15 @@ class ChatApp extends Component {
   }
 
   onSendMessage = (message) => {
+        chatUsername().then((gitHubHandle) => {
+          
+          console.log(gitHubHandle.CurrentUserGitHubHandle + 'in th onsendmessage====================')
+          // const gitHubHandle = chatUsername();
+          this.setState({
+            member: { username: gitHubHandle.CurrentUserGitHubHandle },
+          });
+        });
+
     this.drone.publish({
       room: 'observable-room',
       message,
