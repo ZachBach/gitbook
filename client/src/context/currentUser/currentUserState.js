83403e56993e5df99f50ce077e15395a0d9429e3
Zach Auerbach <auernachzach@gmail.com>
2020-06-18 17:14:28 -0700

    Fixed wall post not working by convering the array to a string wall is refreshing page on post

--- client/src/context/currentUser/currentUserState.js
+++ client/src/context/currentUser/currentUserState.js
@@ -1,59 +1,65 @@
 import React, { useReducer } from 'react';
-import { CurrentUserContext } from './currentUserContext'
+import { CurrentUserContext } from './currentUserContext';
 import { IS_AUTHENTICATED } from '../types';
 import CurrentUserReducer from './currentUserReducer';
-import db from './DexieCurrentUser'
+import db from './DexieCurrentUser';
 
 const CurrentUserState = (props) => {
-    var initialState = {
-        CurrentUserId: null,
-        CurrentUserToken: null,
-        CurrentUserGitHubHandle: null
-    };
+  var initialState = {
+    CurrentUserId: null,
+    CurrentUserToken: null,
+    CurrentUserGitHubHandle: null,
+  };
 
-    const [state, dispatch] = useReducer(CurrentUserReducer, initialState);
+  const [state, dispatch] = useReducer(CurrentUserReducer, initialState);
 
-    db.open().catch((err) => {
-        console.log(err.stack || err)
+  db.open().catch((err) => {
+    console.log(err.stack || err);
+  });
+
+  const updateCurrentUser = async () => {
+    const getCurrentUser = await fetch('/api/currentuser', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json',
+      },
     })
+      .then((data) => data.json())
+      .then((result) => {
+        return result[0];
+      });
 
-    const updateCurrentUser = async () => {
-        const getCurrentUser = await fetch('/api/currentuser', {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json',
-                Accept: 'application/json',
-            },
+    const authenticated = await db.user
+      .where('token')
+      .equals(getCurrentUser.CurrentUserToken)
+      .toString();
+    console.log(authenticated.length);
+    if (authenticated.length < 1) {
+      console.log('You are not Logged in!!');
+      return;
+    } else {
+      db.user
+        .add({
+          token: getCurrentUser.CurrentUserToken,
+          handle: getCurrentUser.CurrentUserGitHubHandle,
         })
-            .then((data) => data.json())
-            .then((result) => {
-                return result[0];
-            })
+        .catch((err) => {
+          console.log(err);
+        });
 
-        const authenticated = await db.user.where("token").equals(getCurrentUser.CurrentUserToken).toArray()
-        console.log(authenticated.length)
-        if (authenticated.length < 1) {
-            console.log("You are not Logged in!!")
-            return
-        } else {
-            db.user.add({ token: getCurrentUser.CurrentUserToken, handle: getCurrentUser.CurrentUserGitHubHandle }).catch((err) => {
-                console.log(err)
-            })
+      dispatch({
+        type: IS_AUTHENTICATED,
+        payload: getCurrentUser,
+      });
+    }
+  };
 
-            dispatch({
-                type: IS_AUTHENTICATED,
-                payload: getCurrentUser
-            });
-        }
-
-
-    };
-
-    return (
-        <CurrentUserContext.Provider value={{ ...state, updateCurrentUser }}>
-            {props.children}
-        </CurrentUserContext.Provider>
-    );
+  return (
+    <CurrentUserContext.Provider value={{ ...state, updateCurrentUser }}>
+      {props.children}
+    </CurrentUserContext.Provider>
+  );
 };
 
 export default CurrentUserState;
