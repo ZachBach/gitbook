026f02a992e5c7de85fbf1d3c41e2f293b6af7ca
Zach Auerbach <auernachzach@gmail.com>
2020-06-19 20:33:46 -0700

    heroku master

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '10416',
-  database: 'users_db',
+  host: 'blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  username: 'nzcmccfn72qhr3cv',
+  password: 'aayfyishw987qywi',
+  database: 'q71io5hpwyg7tpqu',
 });
 
 module.exports = connection;
