b0e85511b48dca046f9246993fb6616808944179
Zach Auerbach <auernachzach@gmail.com>
2023-04-26 07:19:07 -0700

    test

---
+++ client/build/precache-manifest.78391cc01528d92f67727966663556f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69717e486f23daf2908c33ae76ba351d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9b554863404d6de7601",
+    "url": "/static/css/main.8cb18680.chunk.css"
+  },
+  {
+    "revision": "8968b5e128ec3a7b09ac",
+    "url": "/static/js/2.63714f12.chunk.js"
+  },
+  {
+    "revision": "bdcfdbef37b771299e3e4252a0d10375",
+    "url": "/static/js/2.63714f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b554863404d6de7601",
+    "url": "/static/js/main.158751cf.chunk.js"
+  },
+  {
+    "revision": "cc604e02a972c54e967e",
+    "url": "/static/js/runtime-main.05aa69e1.js"
+  },
+  {
+    "revision": "baeaf0c7f02777839e58eed62cc714c4",
+    "url": "/static/media/spinner.baeaf0c7.gif"
+  }
+]);
